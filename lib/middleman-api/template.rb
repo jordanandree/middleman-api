fcaf12d0d16ac9d07ffd5af620ca103472cc41f5
jordanandree <jordan.andree@rootradius.com>
2013-03-05 23:13:07 -0500

    ignore json proxy

--- lib/middleman-api/template.rb
+++ lib/middleman-api/template.rb
@@ -2,13 +2,15 @@ module Middleman::Api
   class Template
     def initialize(app)
       @app = app
+      @proxy_name = "api.json"
+      @app.ignore @proxy_name
     end
 
     # Update the main sitemap resource list
     # @return [void]
     def manipulate_resource_list(resources)
       resources + resources.map do |resource|
-        ::Middleman::Sitemap::Resource.new(@app.sitemap, "api.json", template)
+        ::Middleman::Sitemap::Resource.new(@app.sitemap, @proxy_name, template)
       end
     end
 
