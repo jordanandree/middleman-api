1a24beb90b54e7cfff4505a5144f4d9a7bd526c1
jordanandree <jordan.andree@mailchimp.com>
2015-04-27 14:20:27 -0400

    fix tests failing with encoded html entities in json and xml responses

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,9 @@
 PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
 require "middleman-core"
 require "middleman-core/step_definitions"
+
+# Don't encode html entities for tests
+require 'active_support/json'
+ActiveSupport.escape_html_entities_in_json = false
+
 require File.join(PROJECT_ROOT_PATH, 'lib', 'middleman-api')
